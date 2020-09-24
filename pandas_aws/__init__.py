1a48238c4f94dfcb7dcd3f3f29f38e2aa1f19941
FlorentPajot <florent@bricoprive.com>
2020-09-24 09:30:27 +0200

    [DEV] rework get_client mthod

--- pandas_aws/__init__.py
+++ pandas_aws/__init__.py
@@ -4,13 +4,14 @@ from botocore.exceptions import ProfileNotFound
 import boto3
 
 
-def get_client(service_name: str, profile_name: str = 'default'):
-    """Get AWS client for a specific service while handling credentials via profile"""
+def get_client(service_name: str, profile_name: str = 'default', **kwargs):
+    """Get AWS client for a specific service
+    while handling credentials via profile"""
     try:
         session = boto3.Session(profile_name=profile_name)
-        return session.client(service_name)
+        return session.client(service_name, **kwargs)
     except ProfileNotFound:
-        return boto3.client(service_name=service_name)
+        return boto3.client(service_name=service_name, **kwargs)
 
 
 __all__ = ['s3']
